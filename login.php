06bbbb8a71679c5d141d691658f75b6f5e213413
simoneGuerra0 <148072510+simoneGuerra0@users.noreply.github.com>
2024-02-06 17:23:44 +0100

    ciao

--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Accedi - WikiCar Vintage</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="homepage-style.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Pirata+One&display=swap">
 </head>
 <header>
