316eaabb73b1bcde92e735cc618249b27f5a9935
Mario D'Aniello <115697716+mariodan02@users.noreply.github.com>
2024-02-08 16:50:42 +0100

    Logout fixed

--- logout.php
+++ logout.php
@@ -1,18 +1,4 @@
-<html>
-	<head>
-		<title>WikiCar Vintage Logout</title>
-  	<meta name="author" content="TSW23_09"/>
-  	<meta name="description" content="Archivio di auto d'epoca"/>
-  	<meta charset = "utf-8"/>
-  	<link rel="icon" href="wikicar-logo.png" >
-	<link rel="stylesheet" href="style.css">
-	<link rel="stylesheet" href="headerstyle.css">
-	</head>
-		<?php
-				include 'header.php';
-		?>
-	<body>
-	<br><br><br><br><br><br><br><br><br><br>
+
 		<?php
 			/* distrugge la sessione attiva */
 			$sname=session_name();
@@ -21,12 +7,6 @@
 			if (isset($_COOKIE[session_name()])) {
 				setcookie($sname,'', time()-3600,'/');
 			}
-			echo "<br><br><center><p>Logout effettuato. Ciao ".$_SESSION["username"]." </p>";
-			echo "<br><center><p>Torna alla <a href=\"homepage.php\">Home</a></p>";
+			header('Location: homepage.php'); 
+			exit();
 			?>
-	</body>
-	<br><br><br><br>
-		<?php
-      include 'footer.php';
-    ?>
-</html>
