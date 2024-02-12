c74870a06d3367ba342945be0400764ebbd2c9c2
Mario D'Aniello <115697716+mariodan02@users.noreply.github.com>
2024-02-12 12:20:25 +0100

    Update login-manager.php

--- login-manager.php
+++ login-manager.php
@@ -5,7 +5,8 @@
   	<meta name="description" content="Archivio auto d'epoca"/>
   	<meta charset = "utf-8"/>
   	<link rel="icon" href="wikicar-logo.png" >
-  	<link rel="stylesheet" href=""> 
+	<link rel="stylesheet" href="homepage-style.css">
+    <link rel="stylesheet" href="headerstyle.css">
 	</head>
 	<header style="background-color:white;">
 		<?php
@@ -19,7 +20,7 @@
 				$password =  $_POST['password'];
 				$hash = get_pwd($username);
 				if(!$hash){
-					echo "<br><br><center><p> L'utente $username non esiste. <a href=\"login.php\">Riprova</a> oppure <a href=\"registrazione.php\">registrati adesso!</a> </p>";
+					echo "<br><br><br><br><br><br><br><br><br><br><br><center><p> L'utente $username non esiste. <a href=\"login.php\">Riprova</a> oppure <a href=\"registrazione.php\">registrati adesso!</a> </p>";
 				} else {
 					if (password_verify($password, $hash)) {
 						echo "<script type=\"text/javascript\"> alert(\"Login Eseguito con successo\");</script>";
