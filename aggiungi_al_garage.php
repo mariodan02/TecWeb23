40355a44edcda2b40392e56af542c5e68b50be48
simoneGuerra0 <148072510+simoneGuerra0@users.noreply.github.com>
2024-02-19 16:13:53 +0100

    commenti

--- aggiungi_al_garage.php
+++ aggiungi_al_garage.php
@@ -9,7 +9,7 @@ try {
     // Connessione al database
     $db = pg_connect($connection_string) or die('Impossibile connettersi al database: ' . pg_last_error());
 
-    // Recupera l'ID del garage basato sull'username
+    // Recuperiamo l'ID del garage basato sull'username
     $garageResult = pg_query_params($db, "SELECT id FROM garage WHERE username = $1", array($username));
     if (!$garageResult || pg_num_rows($garageResult) == 0) {
         echo "error: Garage non trovato per l'utente.";
@@ -19,10 +19,10 @@ try {
     $garageRow = pg_fetch_assoc($garageResult);
     $garageId = $garageRow['id'];
 
-    // Verifica che l'auto non sia già nel garage
+    // Verifichiamo che l'auto non sia già nel garage
     $result = pg_query_params($db, "SELECT * FROM garage_auto WHERE auto_id = $1 AND garage_id = $2", array($autoId, $garageId));
     if (!$result) {
-        // Gestire correttamente l'errore della query.
+        // Gestiamo correttamente l'errore della query.
         echo "error: Errore nell'esecuzione della query: " . pg_last_error($db);
         pg_close($db);
         exit;
@@ -31,7 +31,7 @@ try {
     if (pg_num_rows($result) > 0) {
         echo "error: l'auto è già presente nel garage";
     } else {
-        // Inserisci l'auto nel garage
+        // Inseriamo l'auto nel garage
         $insertResult = pg_query_params($db, "INSERT INTO garage_auto (auto_id, garage_id) VALUES ($1, $2)", array($autoId, $garageId));
         if ($insertResult) {
             echo "success";
@@ -40,7 +40,7 @@ try {
         }
     }
 } finally {
-    // Chiudi sempre la connessione al database alla fine del blocco try
+    // Chiudiamo la connessione al database alla fine del blocco try
     if (isset($db) && $db) {
         pg_close($db);
     }
